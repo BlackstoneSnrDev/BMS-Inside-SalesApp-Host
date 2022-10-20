46a07e8e4366c6f52acdb6cf961f9490961852c9
Karla Cardenas <ache92.k@gmail.com>
2022-10-20 06:56:42 -0500

    Working on register tenant page. 5/5 targets accomplished.

--- src/app/partials/register-tenant/register-tenant.component.ts
+++ src/app/partials/register-tenant/register-tenant.component.ts
@@ -21,12 +21,14 @@ import {
 })
 export class RegisterTenant {
   public tenantForm!: FormGroup;
-  public hide: boolean = true;
+  public hide: boolean = false;
+  public coach: any;
 
   public capsStatus: any;
   public items: number[] = [1];
   public uploadedImg: any[] = [];
   public blobURLImg: any;
+  public phoneList: any[] = [0];
 
   constructor(
     private DataService: DataService,
@@ -37,7 +39,11 @@ export class RegisterTenant {
 
   ngOnInit() {
     this.tenantForm = new FormGroup({
-      tenantLogo: new FormControl('', [
+      tenantLogo: new FormControl('defaultVortexLogo', [
+        Validators.required,
+        Validators.minLength(1),
+      ]),
+      tenantCoach: new FormControl('', [
         Validators.required,
         Validators.minLength(1),
       ]),
@@ -45,7 +51,12 @@ export class RegisterTenant {
         Validators.required,
         Validators.minLength(1),
       ]),
+
       adminUsername: new FormControl('', [
+        Validators.required,
+        Validators.minLength(1),
+      ]),
+      adminEmail: new FormControl('', [
         Validators.required,
         Validators.minLength(1),
         Validators.pattern(
@@ -54,41 +65,69 @@ export class RegisterTenant {
       ]),
       adminPassword: new FormControl('', [
         Validators.required,
-        Validators.minLength(4),
-        //Validators.pattern('(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,}'),
+        Validators.minLength(8),
       ]),
       phoneNumber0: new FormControl('', [
         Validators.required,
         Validators.minLength(1),
+        Validators.pattern('[1-9]{1}[0-9]{9}'),
       ]),
     });
-  }
 
-//   capStatus(e: KeyboardEvent) {
-//     let value = this.tenantForm.touched;
-//     if (value && e.getModifierState('CapsLock')) {
-//       this.capsStatus = 'Caps lock is ON.';
-//     } else {
-//       this.capsStatus = 'Caps lock is OFF.';
-//     }
-//   }
+    this.DataService.getMyTDData().subscribe(
+      (response) => {
+        this.coach = response.selectCoach;
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
 
   clonePhoneField() {
     let id = this.items.length;
-    this.tenantForm.addControl(
-      'phoneNumber' + id,
-      new FormControl('', [Validators.required, Validators.minLength(1)])
-    );
+    let valLast = id - 1;
 
-    this.items.push(id);
+    if (this.tenantForm.get('phoneNumber' + valLast)?.value) {
+      this.items.push(id);
+      this.tenantForm.addControl(
+        'phoneNumber' + id,
+        new FormControl('', [
+          Validators.required,
+          Validators.minLength(1),
+          Validators.pattern('[1-9]{1}[0-9]{9}'),
+        ])
+      );
+
+      this.phoneList.push(this.tenantForm.get('phoneNumber' + valLast)?.value);
+      console.log(this.tenantForm);
+      this.hide = false;
+    } else {
+      this.hide = true;
+      setTimeout(() => {
+        this.hide = false;
+      }, 5000);
+    }
   }
 
-  removeClonedPhoneField() {
+  removeClonedPhoneField(indexSection: any) {
     let id = this.items.length - 1;
 
     if (id > 0) {
       this.tenantForm.removeControl('phoneNumber' + id);
-      this.items.pop();
+      this.items.splice(indexSection, 1);
+      this.phoneList.splice(indexSection, 1);
+    }
+  }
+
+  phoneEdited(indexSection: any) {
+    let val = this.tenantForm.get('phoneNumber' + indexSection)?.value;
+    if (this.phoneList[indexSection]) {
+      this.phoneList[indexSection] = val;
+      console.log(this.tenantForm);
+    } else {
+      this.phoneList[indexSection] = val;
+      console.log(this.tenantForm);
     }
   }
 
@@ -121,13 +160,17 @@ export class RegisterTenant {
   }
 
   saveNewTenant() {
-    console.log('save new tenant');
     console.log(this.tenantForm.value);
+    console.log(this.phoneList);
     this.DataService.createNewTenant(this.tenantForm.value.adminUsername, this.tenantForm.value.adminPassword, this.tenantForm.value.tenantName);
     this.messageService.add({
       severity: 'success',
       summary: 'Service Message',
-      detail: 'Tenant created successfully.',
+      detail: 'Tenant was created successfully.',
     });
+    this.tenantForm.reset()
+    this.tenantForm.patchValue({
+      tenantLogo: 'defaultVortexLogo'
+    })
   }
 }
--- src/app/services/services.service.ts
+++ src/app/services/services.service.ts
@@ -1,28 +1,30 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map, Observable, from } from 'rxjs'
+import { map, Observable, from } from 'rxjs';
 import { UsersService } from './auth.service';
 import {
-    AngularFirestore,
-    AngularFirestoreDocument,
-  } from '@angular/fire/compat/firestore'; 
+  AngularFirestore,
+  AngularFirestoreDocument,
+} from '@angular/fire/compat/firestore';
 import { AngularFireFunctions } from '@angular/fire/compat/functions';
-import { arrayUnion, arrayRemove, Timestamp, DocumentReference, DocumentData, deleteField } from '@angular/fire/firestore'
+import {
+  arrayUnion,
+  arrayRemove,
+  Timestamp,
+  DocumentReference,
+  DocumentData,
+  deleteField,
+} from '@angular/fire/firestore';
 import { RandomId } from './services.randomId';
 import firebase from 'firebase/compat/app';
 
 let len = 12;
 let pattern = 'aA0';
 
-
 @Injectable({
-
-  providedIn: 'root'
-
+  providedIn: 'root',
 })
-
 export class DataService {
-
   public navbarDataURL: string;
   public initialTemplateCustomers: string;
 
@@ -34,26 +36,22 @@ export class DataService {
   public currentUid: string;
 
   constructor(
-
     private _http: HttpClient,
     private afs: AngularFirestore,
     private usersService: UsersService,
-    private fns: AngularFireFunctions,
+    private fns: AngularFireFunctions
+  ) {
+    this.navbarDataURL = '../../assets/json/navbar-data.json';
+    this.initialTemplateCustomers =
+      '../../assets/json/initialTemplateCustomers.json';
 
-  ){
-
-    this.navbarDataURL = "../../assets/json/navbar-data.json";
-    this.initialTemplateCustomers = "../../assets/json/initialTemplateCustomers.json";
-
-    this.thURL = "../../assets/json/thData.json";
-    this.tdURL = "../../assets/json/tdData.json";
+    this.thURL = '../../assets/json/thData.json';
+    this.tdURL = '../../assets/json/tdData.json';
 
     this.usersService.userInfo.subscribe(
-        (userInfo) => (this.userInfo = userInfo, console.log(userInfo))
-      );
+      (userInfo) => ((this.userInfo = userInfo), console.log(userInfo))
+    );
     this.currentUid = this.userInfo.uid;
-
-
   }
 
   getMyTHData(): Observable<any> {
@@ -63,114 +61,122 @@ export class DataService {
     return this._http.get(this.tdURL);
   }
 
-createNewTenant(email: any, password: any, tenantName: any) {
-
+  createNewTenant(email: any, password: any, tenantName: any) {
     console.log(this.userInfo.uid);
 
-//  IN DATABASE
-//    create user in firebase auth
+    //  IN DATABASE
+    //    create user in firebase auth
     var config = {
-        apiKey: "AIzaSyBWe_LKvR_EOjbu6mjcDJKFYjohoabNyAM",
-        authDomain: "insidesalesapi.firebaseapp.com",
-        projectId: "insidesalesapi",
+      apiKey: 'AIzaSyBWe_LKvR_EOjbu6mjcDJKFYjohoabNyAM',
+      authDomain: 'insidesalesapi.firebaseapp.com',
+      projectId: 'insidesalesapi',
     };
-    var secondaryApp = firebase.initializeApp(config, "Secondary");
-    const promise = secondaryApp.auth().createUserWithEmailAndPassword(email, password).then((employee: any) => {
+    var secondaryApp = firebase.initializeApp(config, 'Secondary');
+    const promise = secondaryApp
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then((employee: any) => {
         if (employee) {
-            employee.user.updateProfile({
-                displayName: 'testy'
-            })
+          employee.user.updateProfile({
+            displayName: 'testy',
+          });
         }
         let newUid = secondaryApp?.auth()?.currentUser?.uid;
         console.log(newUid);
         secondaryApp.auth().signOut();
-//    create tenant 
-//    add admin uid to tenants uids array
-        this.afs.collection('Tenant').doc(tenantName).set({ 
+        //    create tenant
+        //    add admin uid to tenants uids array
+        this.afs
+          .collection('Tenant')
+          .doc(tenantName)
+          .set({
             tenantName: tenantName,
             uids: [newUid],
-        })
-//    add admin user to users collection
-        this.afs.collection('Tenant').doc(tenantName).collection('users').doc(newUid).set({
+          });
+        //    add admin user to users collection
+        this.afs
+          .collection('Tenant')
+          .doc(tenantName)
+          .collection('users')
+          .doc(newUid)
+          .set({
             tenant: tenantName,
             uid: newUid,
             admin: true,
             // ** add rest of user data here **
-        })
-//    create initial template with just name, emails, and phone numbers 
-        this.afs.collection('Tenant').doc(tenantName).collection('templates').doc('Demo Template').set({ 
-            test: 'test'
-        })
-//    add phone numbers to phoneNumbers collection
+          });
+        //    create initial template with just name, emails, and phone numbers
+        this.afs
+          .collection('Tenant')
+          .doc(tenantName)
+          .collection('templates')
+          .doc('Demo Template')
+          .set({
+            test: 'test',
+          });
+        //    add phone numbers to phoneNumbers collection
 
-//    create status 'active' and 'no active'
+        //    create status 'active' and 'no active'
         const batch = this.afs.firestore.batch();
         let customers = this._http.get(this.initialTemplateCustomers);
         customers.subscribe((data) => {
-            Object.values(data).forEach((customer) => {
-                customer.uid = RandomId(len, pattern);
-                console.log(customer);
-                const ref: any = this.afs
-                .collection('Tenant')
-                .doc(tenantName)
-                .collection('templates')
-                .doc('Demo Template')
-                .collection('customers')
-                .doc(customer.uid).ref;
-              batch.set(ref, {
-                ...customer,
-                group: [],
-                lastContact: null,
-                notes: [],
-                template: 'Demo Template',
-                uid: customer.uid ,
-              });
+          Object.values(data).forEach((customer) => {
+            customer.uid = RandomId(len, pattern);
+            console.log(customer);
+            const ref: any = this.afs
+              .collection('Tenant')
+              .doc(tenantName)
+              .collection('templates')
+              .doc('Demo Template')
+              .collection('customers')
+              .doc(customer.uid).ref;
+            batch.set(ref, {
+              ...customer,
+              group: [],
+              lastContact: null,
+              notes: [],
+              template: 'Demo Template',
+              uid: customer.uid,
+            });
+          });
+          return batch.commit();
+        });
+      });
 
-            })
-            return batch.commit()
-        })
-
-    })  
-
-
-//   add new tenant to currentUser/coach 
-    this.afs.collection('Coach').doc(this.userInfo.uid).update({ 
-        clients: arrayUnion(tenantName)
-    })
+    //   add new tenant to currentUser/coach
+    this.afs
+      .collection('Coach')
+      .doc(this.userInfo.uid)
+      .update({
+        clients: arrayUnion(tenantName),
+      });
     promise.catch(function (e: any) {
-        if (e) {
-            secondaryApp.auth().signOut();
-        }
+      if (e) {
+        secondaryApp.auth().signOut();
+      }
     });
-
-
-        
-    }
-
-
-async getClientList(clientArray: any) {
-        let clientList: any[] = [];
-        await clientArray.forEach((client: string | undefined) => {
-            this.afs.collection('Tenant').doc(client).ref.get().then((docs) => {
-                clientList.push(docs.data());
-            })
-        })
-        return clientList;
-    }
-
-
-  getNavbarData(): Observable<any>{
-
-    return this._http.get(this.navbarDataURL);
-
   }
 
+  async getClientList(clientArray: any) {
+    let clientList: any[] = [];
+    await clientArray.forEach((client: string | undefined) => {
+      this.afs
+        .collection('Tenant')
+        .doc(client)
+        .ref.get()
+        .then((docs) => {
+          clientList.push(docs.data());
+        });
+    });
+    return clientList;
+  }
 
+  getNavbarData(): Observable<any> {
+    return this._http.get(this.navbarDataURL);
+  }
 }
-
-
 
 // Add username to menu like main app
 // phone numbers into array
 // remove caps lock and error message
-// add dropdown of coaches 
+// add dropdown of coaches
